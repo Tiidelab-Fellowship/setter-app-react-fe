df8873946fcdff256e5afb0763a5433b63d31e73
sogobanwo <sogobanwo@gmail.com>
2022-10-18 08:32:33 +0100

    LogIn Page Updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // import logo from './logo.svg';
 // import './App.css';
-// import LoginPage from "./Pages/Login";
+import LoginPage from "./Pages/Login";
 // import SignUpPage from "./Pages/Signup";
 // import ForgotPasswordPage from "./Pages/ForgotPasswordPage";
-import CreateNewPasswordPage from "./Pages/CreateNewPasswordPage";
+// import CreateNewPasswordPage from "./Pages/CreateNewPasswordPage";
 
 
 
 function App() {
   return (
-  //  <LoginPage />
+   <LoginPage />
   //  <SignUpPage />
   //  <ForgotPasswordPage />
-   <CreateNewPasswordPage />
+  //  <CreateNewPasswordPage />
   );
 }
 export default App;
